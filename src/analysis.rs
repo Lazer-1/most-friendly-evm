c0522ccc9da5808bf9b47d9c89b89539430bb01c
9oelM <hj923@hotmail.com>
2023-10-25 23:08:36 +0800

    more

--- src/analysis.rs
+++ src/analysis.rs
@@ -4,3 +4,16 @@
 pub struct BitVec {
     inner: Vec<u8>,
 }
+
+impl BitVec {
+  const SET_2_BITS_MASK: u16 = 0b11;
+  const SET_3_BITS_MASK: u16 = 0b111;
+  const SET_4_BITS_MASK: u16 = 0b1111;
+  const SET_5_BITS_MASK: u16 = 0b1111_1;
+  const SET_6_BITS_MASK: u16 = 0b1111_11;
+  const SET_7_BITS_MASK: u16 = 0b1111_111;
+
+  fn set_1(&mut self, pos: usize) {
+  	self.inner[pos / 8] |= 1 << (pos % 8)
+  }
+}
